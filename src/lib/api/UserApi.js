d83dd42490bec18471dedc9c6bedf0f2dfe484d4
babe25 <babehndr5@gmail.com>
2025-06-03 02:56:20 +0700

    Selesai merge fitur-login ke main

--- src/lib/api/UserApi.js
+++ src/lib/api/UserApi.js
@@ -2,17 +2,6 @@ import axios from "axios";
 
 const endpoint = import.meta.env.VITE_API_PATH;
 
-// export const userRegister = async ({ username, password, name }) => {
-//   return await fetch(`${endpoint}/users`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Accept: "application/json",
-//     },
-//     body: JSON.stringify({ username, password, name }),
-//   });
-// };
-
 export const userRegister = async ({ username, password, name }) => {
   return await axios({
     method: "post",
